908978d027ea76fc518ea041e9e30b3892a07bc9
Riley <ririwilliams.rw@gmail.com>
2024-05-06 18:56:32 -0500

    Added logic for spawnpoints

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ export async function makeMap(k: KaboomCtx, name: string) {
 
     const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
 
-    const spawnPoints: { [key: string]: { x: number, y: number } } = {};
+    const spawnPoints: { [key: string]: { x: number, y: number }[] } = {};
 
     for (const layer of mapData.layers) {
         if (layer.name === "colliders") {
@@ -16,11 +16,28 @@ export async function makeMap(k: KaboomCtx, name: string) {
                         shape: new k.Rect(k.vec2(0), collider.width, collider.height),
                         collisionIgnore: ["platform", "exit"],
                     }),
-                    collider.name !== "exit" ? k.body({isStatic: true}) : null,
+                    collider.name !== "exit" ? k.body({ isStatic: true }) : null,
                     k.pos(collider.x, collider.y),
                     collider.name !== "exit" ? "platform" : "exit"
                 ]);
             }
+            continue;
+        }
+
+        if (layer.name === "spawnpoints") {
+            for (const spawnPoint of layer.objects) {
+                if (spawnPoints[spawnPoint.name]) {
+                    spawnPoints[spawnPoint.name].push({
+                        x: spawnPoint.x,
+                        y: spawnPoint.y,
+                    })
+                    continue;
+                }
+
+                spawnPoints[spawnPoint.name] = [{ x: spawnPoint.x, y: spawnPoint.y }];
+            }
         }
     }
+
+    return {map, spawnPoints};
 }
